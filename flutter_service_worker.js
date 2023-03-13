771729f9c0fa8bb785f468893703bd65e686c7b7
phuoctrandev <thephuoc2101@gmail.com>
2023-03-13 13:49:20 +0700

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,26 +3,27 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "1e98a4f7390d2e9d9ad60c99fb0aff05",
-"index.html": "d5374373fc28f68347dc024996fc279e",
-"/": "d5374373fc28f68347dc024996fc279e",
-"main.dart.js": "a73d67fc42fde6d09ef13dfb0f67bb43",
-"flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
+  "version.json": "009c9e65172e010890f7f65fde438006",
+"index.html": "228565ce661a2d44541f498121545bce",
+"/": "228565ce661a2d44541f498121545bce",
+"main.dart.js": "3c9f023cd2a3fa63e2a104693d35d5b6",
+"flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"manifest.json": "c81f25a8d1362ede544c1913a9c96ac1",
+"manifest.json": "d40c47d1c161f94dbcb13094d37f1f55",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "951bd995f59b858c05905cefe6c73466",
+"assets/NOTICES": "4e884eb0e3293928de1f120ba398b903",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
-"canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
-"canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
-"canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
-"canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e"
+"assets/shaders/ink_sparkle.frag": "5804f32013449095f556ca24ebe4f168",
+"assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
+"canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
+"canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
+"canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
+"canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62"
 };
 
 // The application shell files that are downloaded before a service worker can
