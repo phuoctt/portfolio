d8a68f4856a1d93fa3945f29988558bba9693d23
PhuocTT <thephuoc2101@gmail.com>
2023-03-20 21:19:22 +0700

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "a29e8b22a08aa0274b027931eec216c4",
-"index.html": "35426d5db8fdb910214ba58912011e42",
-"/": "35426d5db8fdb910214ba58912011e42",
+"index.html": "83bb5063924e350202190f080003b588",
+"/": "83bb5063924e350202190f080003b588",
 "main.dart.js": "f60a5472a936320b32a9be3de68aadaa",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "50ec8d1ca2331826e900004b6adf6674",
